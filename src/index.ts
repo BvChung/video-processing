2a499ebbc74a57efaf709ba4c8f8c60bb11d3c1d
Brandon Chung <chung.brandon123@gmail.com>
2023-08-31 23:59:54 -0500

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import pc from "picocolors";
-import { setupDirectories } from "./processing-video/processingUtils";
+import { setupDirectories } from "./processing-video/service";
 import processVideoRoute from "./processing-video/route";
+import serverHealthRoute from "./server-health/route";
 
 dotenv.config();
 setupDirectories();
 
 const app = express();
 const port = process.env.PORT || 4000;
-// const logger = pinoHttp();
 
 app.use(express.json());
-// app.use(logger);
 
 app.get("/", (req, res) => {
 	res.status(200).json({ params: req.params, query: req.query });
 });
 
 app.use("/api/v1/process-video", processVideoRoute);
+app.use("/api/v1/health", serverHealthRoute);
 
 app.listen(port, () => {
 	console.log(
